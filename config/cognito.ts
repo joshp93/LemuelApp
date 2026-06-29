93eccf0dee6717394215632a5da3277e80098ff4
Joshua Pearson <joshuapearson1993@gmail.com>
2026-06-29 23:02:08 +0100

    add cognito config for build

---
+++ config/cognito.ts
@@ -0,0 +1,10 @@
+/**
+ * Cognito configuration
+ * DO NOT commit this file to git - use cognito.example.ts as template
+ */
+
+export const COGNITO_CONFIG = {
+  region: "eu-west-2",
+  userPoolId: "eu-west-2_Oas7XphX7",
+  clientId: "557lc1t882n3uf3o8ra8cu03oa",
+} as const;
